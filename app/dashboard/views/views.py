c21e553fe7e04caa8af6e62be8e8ba125fa23e8a
lucky <lucky19870315@163.com>
2016-06-13 17:22:58 +0800

    commit

--- app/dashboard/views/views.py
+++ app/dashboard/views/views.py
@@ -4,6 +4,6 @@ from flask import render_template
 from app.dashboard import dashboard
 
 
-@dashboard.route('/test', methods=['GET'])
-def get_resource():
+@dashboard.route('/index', methods=['GET'])
+def index():
     return render_template("dashboard/index.html")
